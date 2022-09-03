06d6da4e60ddaf62567f6b5fb5dbe365c3e5d31a
Simon Chenery <simon_chenery@yahoo.com>
2022-09-03 18:17:18 +0200

    Add '__' prefix to mark ValueArray members as private

--- valuearray.py
+++ valuearray.py
@@ -3,20 +3,20 @@ class ValueArray:
 	"""Array of constants that are used by a chunk"""
 
 	def __init__(self):
-		self.values = []
+		self.__values = []
 
 	def __getitem__(self, item):
 		"""Get constant from array by index"""
-		return self.values[item]
+		return self.__values[item]
 
 	def writeValueArray(self, value):
 		"""Add a constant to array"""
-		self.values.append(value)
+		self.__values.append(value)
 
 	def clear(self):
 		"""Clear all constants"""
-		self.values.clear()
+		self.__values.clear()
 
 	def len(self):
 		"""Return number of contants in array"""
-		return len(self.values)
+		return len(self.__values)
