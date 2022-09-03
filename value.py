c24f27d91f61518daa9ac01375fb7985c7786eff
Simon Chenery <simon_chenery@yahoo.com>
2022-09-03 19:29:58 +0200

    Add '__' prefix to mark Value members as private

--- value.py
+++ value.py
@@ -9,68 +9,68 @@ class ValueType(IntEnum):
 
 class Value:
 	def __init__(self):
-		self.type = ValueType.VAL_NIL
-		self.boolean = False
-		self.number = 0
-		self.obj = None
+		self.__type = ValueType.VAL_NIL
+		self.__boolean = False
+		self.__number = 0
+		self.__obj = None
 
 	def BOOL_VAL(n):
 		value = Value()
-		value.type = ValueType.VAL_BOOL
-		value.boolean = n
+		value.__type = ValueType.VAL_BOOL
+		value.__boolean = n
 		return value
 
 	def NIL_VAL():
 		value = Value()
-		value.type = ValueType.VAL_NIL
+		value.__type = ValueType.VAL_NIL
 		return value
 
 	def NUMBER_VAL(n):
 		value = Value()
-		value.type = ValueType.VAL_NUMBER
-		value.number = n
+		value.__type = ValueType.VAL_NUMBER
+		value.__number = n
 		return value
 
 	def OBJ_VAL(n):
 		value = Value()
-		value.type = ValueType.VAL_OBJ
-		value.obj = n
+		value.__type = ValueType.VAL_OBJ
+		value.__obj = n
 		return value
 
 	def IS_BOOL(self):
-		return self.type == ValueType.VAL_BOOL
+		return self.__type == ValueType.VAL_BOOL
 
 	def IS_NIL(self):
-		return self.type == ValueType.VAL_NIL
+		return self.__type == ValueType.VAL_NIL
 
 	def IS_NUMBER(self):
-		return self.type == ValueType.VAL_NUMBER
+		return self.__type == ValueType.VAL_NUMBER
 
 	def IS_OBJ(self):
-		return self.type == ValueType.VAL_OBJ
+		return self.__type == ValueType.VAL_OBJ
 
 	def AS_BOOL(self):
-		return self.boolean
+		return self.__boolean
 
 	def AS_NUMBER(self):
-		return self.number
+		return self.__number
 
 	def AS_OBJ(self):
-		return self.obj
+		return self.__obj
 
 	def printValue(value):
 		print('{0:g}'.format(value.AS_NUMBER()), end='')
 
 	def valuesEqual(a, b):
-		if a.type != b.type:
+		if a.__type != b.__type:
 			return False
-		if a.type == ValueType.VAL_BOOL:
-			return a.boolean == b.boolean
-		if a.type == ValueType.VAL_NIL:
+		if a.__type == ValueType.VAL_BOOL:
+			return a.__boolean == b.__boolean
+		if a.__type == ValueType.VAL_NIL:
 			return True
-		if a.type == ValueType.VAL_NUMBER:
-			return a.number == b.number
-		if a.type == ValueType.VAL_OBJ:
+		if a.__type == ValueType.VAL_NUMBER:
+			return a.__number == b.__number
+		if a.__type == ValueType.VAL_OBJ:
 			aString = a.AS_OBJ().AS_STRING()
 			bString = b.AS_OBJ().AS_STRING()
 			return aString == bString
